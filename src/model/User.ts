4b2176542fcd7469e668dbbdb5ce4e474b045b57
osamarizwan98 <osamarizwan98@gmail.com>
2024-06-06 19:48:57 +0500

    upload initial files

---
+++ src/model/User.ts
@@ -0,0 +1,65 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+// User Schema
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessages: boolean;
+  messages: Message[]
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Verify code is required"],
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: true,
+    },
+    isAcceptingMessages: {
+        type: Boolean,
+        default: false,
+    },
+    messages: [MessageSchema]
+});
+
+const UserModal = 
+    (mongoose.models.User as mongoose.Model<User> ) || 
+    mongoose.model('User', UserSchema)
